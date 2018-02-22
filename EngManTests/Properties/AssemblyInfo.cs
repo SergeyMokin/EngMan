819bdde5c3d02fcf5664fb52d1a712c227779294
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-02-22 14:58:46 +0300

    created testproject

---
+++ EngManTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EngManTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("EngManTests")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("20a66612-018b-444d-9712-50bb9f6c4a9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EngManTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EngMan.Controllers;
+using EngMan.Models;
+using EngMan.Service;
+using EngMan.Repository;
+using System.Web.Http;
+namespace EngManTests
+{
+    [TestClass]
+    public class TestSimpleProductController
+    {
+        [TestMethod]
+        public void GetAllProducts()
+        {
+        }
+    }
+}
