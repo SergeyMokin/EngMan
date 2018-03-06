11164dfb9e3779c13f4119333b41a748ca04b4c5
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-03-06 19:05:47 +0300

    Fixes #6

--- EngMan/Extensions/Extensions.cs
+++ EngMan/Extensions/Extensions.cs
@@ -248,20 +248,7 @@ namespace EngMan.Extensions
             {
                 buffer4 = bytes.GetBytes(0x20);
             }
-            return buffer3.IsEqual(buffer4);
-        }
-
-        //comparison of byte arrays
-        private static bool IsEqual(this byte[] b1, byte[] b2)
-        {
-            if (b1 == b2) return true;
-            if (b1 == null || b2 == null) return false;
-            if (b1.Length != b2.Length) return false;
-            for (int i = 0; i < b1.Length; i++)
-            {
-                if (b1[i] != b2[i]) return false;
-            }
-            return true;
+            return buffer3.SequenceEqual(buffer4);
         }
     }
 }
