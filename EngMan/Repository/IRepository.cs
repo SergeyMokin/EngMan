cb5c4187da05f7f5249e70265a47df0f0e40ea46
MINSK\Siarhei_Mokin <Siarhei_Mokin@epam.com>
2018-03-20 21:15:33 +0300

    singled out the general essence

---
+++ EngMan/Repository/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace EngMan.Repository
+{
+    public interface IRepository<ReturnType, ParameterType>
+    {
+        IQueryable<ReturnType> GetAll();
+
+        ReturnType Get(int id);
+
+        bool Add(ParameterType value);
+
+        bool Edit(ParameterType value);
+
+        int Delete(int id);
+
+        IQueryable<string> GetAllCategories();
+
+        IQueryable<ReturnType> GetByCategory(string category);
+    }
+}
